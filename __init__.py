4cce35d1da52dd1d93057aee2b01fa1545d3d18f
bahga <43752565+bahga@users.noreply.github.com>
2020-02-03 14:46:55 -0500

    Restructured folders

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 14 13:27:29 2018
-
-@author: glenabastillas
-"""
-
---
+++ board/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 13:27:29 2018
+
+@author: glenabastillas
+"""
+
